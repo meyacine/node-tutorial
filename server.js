deb1fa7927833d027d95ab0541db203b2c737f97
Maamar Yacine MEDDAH <my.meddah@gmail.com>
2016-02-23 17:18:12 +0100

    changed to write method and end

--- server.js
+++ server.js
@@ -9,7 +9,8 @@ var http = require('http');
  */
 var init = function(request, result) {
     result.writeHead(200, {"Content-Type": "application/json"});
-    result.end('Salut tout le monde !');
+    result.write('Salut tout le monde !');
+    result.end();
 }
 var server = http.createServer(init);
 server.listen(80);
